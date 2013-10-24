0af2cd5e048fff462beffd5f58bc9e15efddf956
actionless <actionless.loveless@gmail.com>
2013-10-24 17:28:33 +0300

    small fix

--- newscoop/copy_images_from_lab.sh
+++ newscoop/copy_images_from_lab.sh
@@ -7,5 +7,5 @@ test "$BRANCH" = 'wobs-stable' && REMOTE_PATH="/var/www/tw-pre-live/images"
 test $APP = "tw" && LOCAL_PATH="/var/dumps/""$APP""_images/""$BRANCH"
 
 mkdir -p "$LOCAL_PATH"
-rsync -av -e "ssh -i /root/.ssh/bamboo -p2209" bamboo@lab.sourcefabric.org:$REMOTE_PATH $LOCAL_PATH --exclude='image_cache'
+rsync -av -e "ssh -i /root/.ssh/bamboo -p2209" bamboo@lab.sourcefabric.org:$REMOTE_PATH $LOCAL_PATH --exclude image_cache/
 
