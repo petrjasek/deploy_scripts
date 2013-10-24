9b88a3a5b7706d17e75505bd4d44ced5d3098fa5
actionless <actionless.loveless@gmail.com>
2013-10-24 17:25:26 +0300

    added scipt for copying images from LAB

---
+++ newscoop/copy_images_from_lab.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#default path for tw:
+test $APP = "tw" && REMOTE_PATH="/var/www/tw-reloaded/images"
+test "$BRANCH" = 'wobs-stable' && REMOTE_PATH="/var/www/tw-pre-live/images"
+
+test $APP = "tw" && LOCAL_PATH="/var/dumps/""$APP""_images/""$BRANCH"
+
+mkdir -p "$LOCAL_PATH"
+rsync -av -e "ssh -i /root/.ssh/bamboo -p2209" bamboo@lab.sourcefabric.org:$REMOTE_PATH $LOCAL_PATH
+
