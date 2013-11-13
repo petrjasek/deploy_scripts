69f6d5e5a65ace325de4aac69eea94b31a3735f6
actionless <actionless.loveless@gmail.com>
2013-11-13 18:34:35 +0300

    added unarchivate to script

--- nsc/copy_db_from_lab.sh
+++ nsc/copy_db_from_lab.sh
@@ -6,5 +6,9 @@ REMOTE_PATH="\'/var/www/""$APP""-prelive/backup/backup-*.tar.gz\'"
 LOCAL_PATH="/var/dumps/""$APP""/"
 
 mkdir -p "$LOCAL_PATH"
+cd "$LOCAL_PATH"
+rm backup*.tar.gz
 rsync -av -e "ssh -i /root/.ssh/bamboo -p2209" bamboo@lab.sourcefabric.org:$REMOTE_PATH $LOCAL_PATH
+tar xvf backup*.tar.gz
+ls -laho
 
