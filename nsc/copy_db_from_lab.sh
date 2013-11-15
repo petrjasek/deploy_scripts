42ad5ff59891e44b24d357286c33018dffc086c1
actionless <actionless.loveless@gmail.com>
2013-11-15 19:51:42 +0300

    removed dquotes from copy db script for container

--- nsc/copy_db_from_lab.sh
+++ nsc/copy_db_from_lab.sh
@@ -7,6 +7,6 @@ PORT="$bamboo_port"
 REMOTE_PATH="/var/www/""$APP""-prelive/backup/backup-*.tar.gz"
 LOCAL_PATH="/var/dumps/""$APP""/backup.tar.gz"
 
-echo rsync -a --rsh="ssh -p$PORT" --protect-args --rsync-path="sudo rsync" "$REMOTE_PATH" lab@"$DEVELOPER".sourcefabric.net:"$LOCAL_PATH"
-rsync -a --rsh="ssh -p$PORT" --protect-args --rsync-path="sudo rsync" "$REMOTE_PATH" lab@"$DEVELOPER".sourcefabric.net:"$LOCAL_PATH"
+echo rsync -a --rsh="ssh -p$PORT" --protect-args --rsync-path="sudo rsync" $REMOTE_PATH lab@"$DEVELOPER".sourcefabric.net:"$LOCAL_PATH"
+rsync -a --rsh="ssh -p$PORT" --protect-args --rsync-path="sudo rsync" $REMOTE_PATH lab@"$DEVELOPER".sourcefabric.net:"$LOCAL_PATH"
 
