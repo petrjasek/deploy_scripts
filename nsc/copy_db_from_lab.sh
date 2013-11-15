5663d01e77b2b6192c1e6e1fd6cda00011e99814
actionless <actionless.loveless@gmail.com>
2013-11-15 19:30:40 +0300

    fixed path in  copy db script for container

--- nsc/copy_db_from_lab.sh
+++ nsc/copy_db_from_lab.sh
@@ -4,7 +4,7 @@ APP="$bamboo_app"
 DEVELOPER="$bamboo_developer"
 PORT="$bamboo_port"
 
-REMOTE_PATH="\'/var/www/""$APP""-prelive/backup/backup-*.tar.gz\'"
+REMOTE_PATH="/var/www/""$APP""-prelive/backup/backup-*.tar.gz"
 LOCAL_PATH="/var/dumps/""$APP""/backup.tar.gz"
 
 rsync -a --rsh="ssh -p$PORT " --rsync-path="sudo rsync" "$REMOTE_PATH" lab@"$DEVELOPER".sourcefabric.net:"$LOCAL_PATH"
