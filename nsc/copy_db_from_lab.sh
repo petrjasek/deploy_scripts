a2a74d678319db62762db023d8b9467fc2799c2a
actionless <actionless.loveless@gmail.com>
2013-11-15 19:43:02 +0300

    reverting escaping to copy db script for container

--- nsc/copy_db_from_lab.sh
+++ nsc/copy_db_from_lab.sh
@@ -8,5 +8,5 @@ REMOTE_PATH="/var/www/""$APP""-prelive/backup/backup-*.tar.gz"
 LOCAL_PATH="/var/dumps/""$APP""/backup.tar.gz"
 
 echo rsync -a --rsh="ssh -p$PORT" --protect-args --rsync-path="sudo rsync" "$REMOTE_PATH" lab@"$DEVELOPER".sourcefabric.net:"$LOCAL_PATH"
-rsync -a --rsh=\"ssh -p$PORT\" --protect-args --rsync-path="sudo rsync" "$REMOTE_PATH" lab@"$DEVELOPER".sourcefabric.net:"$LOCAL_PATH"
+rsync -a --rsh="ssh -p$PORT" --protect-args --rsync-path="sudo rsync" "$REMOTE_PATH" lab@"$DEVELOPER".sourcefabric.net:"$LOCAL_PATH"
 
