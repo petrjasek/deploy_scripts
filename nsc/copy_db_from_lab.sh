0ed530db11c0c9be2204add765aeb4a711e24a5c
actionless <actionless.loveless@gmail.com>
2013-11-15 19:38:45 +0300

    trying to fix wildcards in copy db script for container

--- nsc/copy_db_from_lab.sh
+++ nsc/copy_db_from_lab.sh
@@ -7,5 +7,5 @@ PORT="$bamboo_port"
 REMOTE_PATH="/var/www/""$APP""-prelive/backup/backup-*.tar.gz"
 LOCAL_PATH="/var/dumps/""$APP""/backup.tar.gz"
 
-rsync -a --rsh="ssh -p$PORT " --rsync-path="sudo rsync" "$REMOTE_PATH" lab@"$DEVELOPER".sourcefabric.net:"$LOCAL_PATH"
+rsync -a --rsh="ssh -p$PORT" --protect-args --rsync-path="sudo rsync" "$REMOTE_PATH" lab@"$DEVELOPER".sourcefabric.net:"$LOCAL_PATH"
 
