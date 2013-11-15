7e27134609179697864ddb4873b00a64ce9eec0c
actionless <actionless.loveless@gmail.com>
2013-11-15 19:26:36 +0300

    updated copy db script for container

--- nsc/copy_db_from_lab.sh
+++ nsc/copy_db_from_lab.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
 
 APP="$bamboo_app"
+DEVELOPER="$bamboo_developer"
+PORT="$bamboo_port"
 
 REMOTE_PATH="\'/var/www/""$APP""-prelive/backup/backup-*.tar.gz\'"
-LOCAL_PATH="/var/dumps/""$APP""/"
+LOCAL_PATH="/var/dumps/""$APP""/backup.tar.gz"
 
-mkdir -p "$LOCAL_PATH"
-cd "$LOCAL_PATH"
-rm backup*.tar.gz
-rsync -av -e "ssh -i /root/.ssh/bamboo -p2209" bamboo@lab.sourcefabric.org:$REMOTE_PATH $LOCAL_PATH
-tar xvf backup*.tar.gz
-ls -laho
+rsync -a --rsh="ssh -p$PORT " --rsync-path="sudo rsync" "$REMOTE_PATH" lab@"$DEVELOPER".sourcefabric.net:"$LOCAL_PATH"
 
