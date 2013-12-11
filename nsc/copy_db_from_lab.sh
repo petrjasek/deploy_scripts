0f128851f99697312bcdfd9e2c6ef25d0c53e45d
actionless <actionless.loveless@gmail.com>
2013-12-11 14:23:02 +0300

    added mkdir before rsync for backup copying - 2

--- nsc/copy_db_from_lab.sh
+++ nsc/copy_db_from_lab.sh
@@ -7,7 +7,7 @@ PORT="$bamboo_port"
 REMOTE_PATH="/var/www/""$APP""-prelive/backup/backup-*.tar.gz"
 LOCAL_PATH="/var/dumps/""$APP""/backup.tar.gz"
 
-ssh -p$PORT lab@"$DEVELOPER".sourcefabric.net -x "mkdir -p /var/dumps/$APP"
+ssh -p$PORT lab@"$DEVELOPER".sourcefabric.net -x "sudo mkdir -p /var/dumps/$APP"
 echo rsync -a --rsh="ssh -p$PORT" --protect-args --rsync-path="sudo rsync" $REMOTE_PATH lab@"$DEVELOPER".sourcefabric.net:"$LOCAL_PATH"
 rsync -a --rsh="ssh -p$PORT" --protect-args --rsync-path="sudo rsync" $REMOTE_PATH lab@"$DEVELOPER".sourcefabric.net:"$LOCAL_PATH"
 
