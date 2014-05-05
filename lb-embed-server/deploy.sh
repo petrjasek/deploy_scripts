ecc50f367ed3901a75a174e21c6d4c1a3bae3e95
actionless <actionless.loveless@gmail.com>
2014-05-05 15:42:28 +0200

    lb-embed-server: added config.js template; updated deploy script

---
+++ lb-embed-server/deploy.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+root=/var/www/nodejs-dev
+
+backend_url="$1"
+backend_port="$2"
+frontend_url="$3"
+[ "$2" = "" ] &&
+		echo "Usage: $0 backend_url backend_port [frontend_url]" &&
+		exit 1
+[ "$3" = "" ] &&
+		frontend_url=$backend_url
+
+# prepare and copy srcs
+working_dir=$(dirname $0)
+mkdir -p $root 2>/dev/null
+cp -rf * .* $root/
+cd $root
+
+# generate config
+source $working_dir/files/config.js.tpl > config.js
+
+# install deps
+npm --allow-root install
+bower --allow-root install
+
+# restart server
+grunt server:forever:restart
