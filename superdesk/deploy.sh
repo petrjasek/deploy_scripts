4756e8cd893f9345db0f95402cb80227d6a90c3c
actionless <actionless.loveless@gmail.com>
2014-05-13 14:48:17 +0000

    superdesk: added copy

--- superdesk/deploy.sh
+++ superdesk/deploy.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-[ -z $1 ] && echo "Usage: $0 instance_name" && exit 1
+[ -z $2 ] && echo "Usage: $0 instance_name src_path" && exit 1
 
 instance="$1"
+src_path="$2"
 instance_path=/var/opt/superdesk_instances/$instance
 backend_path=$instance_path/backend
 
 # create working directory
 mkdir -p $backend_path
+cp -fr $src_path/{*,.*} $backend_path/
 
 # create/reuse virtual environment
 [ ! -f $instance_path/env/bin/activate ] && (
