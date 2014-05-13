9bb52ca11944080bf3355344ef3908aaa5a659c3
actionless <actionless.loveless@gmail.com>
2014-05-13 15:44:41 +0000

    superdesk: fixed venv

--- superdesk/deploy.sh
+++ superdesk/deploy.sh
@@ -7,18 +7,13 @@ src_path="$2"
 instance_path=/var/opt/superdesk_instances/$instance
 backend_path=$instance_path/backend
 
-# create working directory
-mkdir -p $backend_path
-echo cp -fr $src_path $backend_path/
-cp -fr $src_path $backend_path/
-#cd $instance_path
-#mv superdesk backend
 
-exit 0
+rm -fr $backend_path
+cp -fr $src_path $instance_path/backend
 
 # create/reuse virtual environment
 [ ! -f $instance_path/env/bin/activate ] && (
-    virtualenv -p python3.2 $instance_path/env;
+    virtualenv-3.4 -p python3.3 $instance_path/env;
 )
 . $instance_path/env/bin/activate;
 
