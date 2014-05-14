f5179af271a89a7f0b685cfba5e88e6488982a5d
actionless <actionless.loveless@gmail.com>
2014-05-14 16:43:21 +0200

    superdesk: added conditions for backend deploy script

--- superdesk/backend_deploy.sh
+++ superdesk/backend_deploy.sh
@@ -12,24 +12,32 @@ BACKEND_PATH=$INSTANCE_PATH/backend
 
 
 # flush src dir, copy fresh one and go there
-rm -fr $BACKEND_PATH
-cp -fr $SRC_PATH $BACKEND_PATH
-cd $BACKEND_PATH
+(
+	rm -fr $BACKEND_PATH ;
+	cp -fr $SRC_PATH $BACKEND_PATH &&
+	cd $BACKEND_PATH
+) &&
 
 # create/reuse virtual environment
 [ ! -f $INSTANCE_PATH/env/bin/activate ] && (
     virtualenv-3.4 -p python3.3 $INSTANCE_PATH/env;
 )
-. $INSTANCE_PATH/env/bin/activate;
+. $INSTANCE_PATH/env/bin/activate &&
 
 # install dependencies
-pip install -U pip distribute
-pip install -U -r $BACKEND_PATH/requirements.txt
-pip install -U gunicorn
+(
+	pip install -U pip distribute &&
+	pip install -U -r $BACKEND_PATH/requirements.txt &&
+	pip install -U gunicorn
+) &&
 
 # restart application
-supervisorctl update
-supervisorctl restart superdesk_$INSTANCE
+(
+	supervisorctl update ;
+	supervisorctl restart superdesk_$INSTANCE
+) &&
 
 # create admin user
-python manage.py users:create -u admin -p admin 2>&1 && exit 0
+python manage.py users:create -u admin -p admin 2>&1 ;
+
+exit 0
