85d24d58e077c7499696fb45223bf7a590ad037a
actionless <actionless.loveless@gmail.com>
2014-05-14 17:31:31 +0200

    superdesk: added few comments and restructurized instance_delete

--- superdesk/instance_delete.sh
+++ superdesk/instance_delete.sh
@@ -2,17 +2,30 @@
 
 [ -z "$1" ] && echo "Usage: $0 instance_name" && exit 1
 
+
 INSTANCE="$1"
 ROOT=/var/opt/superdesk_instances
 INSTANCE_ROOT=$ROOT/$INSTANCE
 
-rm -r $INSTANCE_ROOT
 
-rm /etc/nginx/sites-enabled/superdesk_$INSTANCE
-service nginx reload
+# remove all the instance files
+rm -r $INSTANCE_ROOT ;
 
-rm /etc/supervisor/conf.d/superdesk_$INSTANCE.conf
-supervisorctl reread
-supervisorctl update
+# remove nginx vhost
+(
+	rm /etc/nginx/sites-enabled/superdesk_$INSTANCE &&
+	service nginx reload
+) ;
 
-rm -r /var/www/sd-test/$INSTANCE/
+# remove supervisor config and shut down instance if running
+(
+	rm /etc/supervisor/conf.d/superdesk_$INSTANCE.conf &&
+	supervisorctl reread &&
+	supervisorctl update ||
+	exit 2
+) ;
+
+# remove index page
+rm -r /var/www/sd-test/$INSTANCE/ ;
+
+exit 0
