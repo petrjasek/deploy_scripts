84918547ab80ba1bbb43cc40c814d0b2892270e7
actionless <actionless.loveless@gmail.com>
2014-05-16 16:56:28 +0200

    superdesk: frontend: added unit tests scripts

--- superdesk/frontend_unit.sh
+++ superdesk/frontend_unit.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+##!/bin/sh
 
 [ -z "$2" ] &&
 echo "Usage: $0 INSTANCE_NAME INSTANCE_URL" &&
@@ -10,8 +10,14 @@ INSTANCE_NAME="$1"
 URL="$2"
 ROOT_PATH=/var/opt/superdesk_instances/$INSTANCE_NAME
 FRONTEND_PATH=$ROOT_PATH/frontend
+PWD=$(readlink -e $(dirname $0))
 
 cd $FRONTEND_PATH &&
-grunt build --server="$URL" &&
+
+# run tests
+grunt ci:bamboo &&
+
+mkdir -p $PWD/../../results/ 2> /dev/null ;
+mv server-test-results.xml client-test-results.xml $PWD/../../results/ &&
 
 exit 0
